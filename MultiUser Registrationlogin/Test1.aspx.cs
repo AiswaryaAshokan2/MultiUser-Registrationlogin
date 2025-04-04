3a63674095cdfa214322f3323c84047def943388
AiswaryaAshokan2 <aiswaryasreenath2024@gmail.com>
2025-04-04 11:18:13 +0530

    multiUser

---
+++ MultiUser Registrationlogin/Test1.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MultiUser_Registrationlogin
+{
+    public partial class Test1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
